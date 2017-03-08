8c83c83528584fd9af9c162e3de56754b2857223
Jean Piero Hernandez Meze <jeanpierohm@gmail.com>
2017-03-08 15:53:04 +0100

    fix: add DJANGO_ENV for heroku in manage.py

--- manage.py
+++ manage.py
@@ -3,7 +3,9 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "microsite_backend.settings")
+    django_env = os.getenvb('DJANGO_ENV', '')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE",
+                          "microsite_backend.{}settings".format(django_env))
 
     from django.core.management import execute_from_command_line
 
